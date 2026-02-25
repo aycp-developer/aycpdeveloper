008c7eb64b7532b55e7e2195c37ef3ffdee61d62
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2026-02-25 12:37:43 -0600

    videos de coding y algorithms agregados

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,7 +4,8 @@ import profilePicture from './aycpdeveloper-photos/aycp-developer-profile.jpg';
 import frontend from './tech-stack/frontend.png';
 import backend from './tech-stack/backend.png';
 import backgroundFooter from './background-footer/background-footer.png';
-import codingVideo from './coding-video/coding.mp4';
+import coding from './coding-algorithms/coding.mp4';
+import algorithms from './coding-algorithms/algorithms.mp4';
 
 export {
     aycpdeveloperLogo,
@@ -13,5 +14,6 @@ export {
     frontend,
     backend,
     backgroundFooter,
-    codingVideo
+    coding,
+    algorithms
 };
